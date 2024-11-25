354de269b1997b95fff72a86920b250056fef57a
nefrttUnknown <39979965+nefrttPrabhu@users.noreply.github.com>
2024-11-25 13:05:34 +0530

    Update union of sorted.cpp

--- 18/union of sorted.cpp
+++ 18/union of sorted.cpp
@@ -1,4 +1,10 @@
-
+/*
+int arr1[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+int arr2[] = {2, 3, 4, 4, 5, 11, 12}; 
+Output:
+Union of arr1 and arr2 is but we dont repeat any element
+1 2 3 4 5 6 7 8 9 10 11 12. 
+*/
 
 
 #include <bits/stdc++.h>
@@ -14,7 +20,8 @@ vector < int > FindUnion(int arr1[], int arr2[], int n, int m) {
       if (Union.size() == 0 || Union.back() != arr1[i])
         Union.push_back(arr1[i]);
       i++;
-    } else // case 3
+    } 
+    else // case 3
     {
       if (Union.size() == 0 || Union.back() != arr2[j])
         Union.push_back(arr2[j]);
@@ -34,18 +41,5 @@ vector < int > FindUnion(int arr1[], int arr2[], int n, int m) {
     j++;
   }
   return Union;
-}
-
-int main()
-
-{
-  int n = 10, m = 7;
-  int arr1[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-  int arr2[] = {2, 3, 4, 4, 5, 11, 12}; 
-  vector < int > Union = FindUnion(arr1, arr2, n, m);
-  cout << "Union of arr1 and arr2 is  " << endl;
-  for (auto & val: Union)
-    cout << val << " ";
-  return 0;
 }
 
