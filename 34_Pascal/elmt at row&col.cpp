882ef3b28e05f40509ac1ca169f9e0a506e0c257
nefrttUnknown <39979965+nefrttPrabhu@users.noreply.github.com>
2024-11-27 15:40:09 +0530

    Update elmt at row&col.cpp

--- 34_Pascal/elmt at row&col.cpp
+++ 34_Pascal/elmt at row&col.cpp
@@ -1,4 +1,3 @@
-// Brute force
 int nCr(int n, int r) {
     long long res = 1;
 
@@ -7,45 +6,7 @@ int nCr(int n, int r) {
         res = res * (n - i);
         res = res / (i + 1);
     }
-    return (int)(res);
-}
-
-vector<vector<int>> pascalTriangle(int n) {
-    vector<vector<int>> ans;
-
-    //Store the entire pascal's triangle:
-    for (int row = 1; row <= n; row++) {
-        vector<int> tempLst; // temporary list
-        for (int col = 1; col <= row; col++) {
-            tempLst.push_back(nCr(row - 1, col - 1));
-        }
-        ans.push_back(tempLst);
-    }
-    return ans;
+    return res;
 }
 
 
-// optimal
-vector<int> generateRow(int row) {
-    long long ans = 1;
-    vector<int> ansRow;
-    ansRow.push_back(1); //inserting the 1st element
-
-    //calculate the rest of the elements:
-    for (int col = 1; col < row; col++) {
-        ans = ans * (row - col);
-        ans = ans / col;
-        ansRow.push_back(ans);
-    }
-    return ansRow;
-}
-
-vector<vector<int>> pascalTriangle(int n) {
-    vector<vector<int>> ans;
-
-    //store the entire pascal's triangle:
-    for (int row = 1; row <= n; row++) {
-        ans.push_back(generateRow(row));
-    }
-    return ans;
-}
