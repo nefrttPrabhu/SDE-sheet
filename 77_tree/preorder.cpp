1f2ca4c0a2dad11a5aaf61861d8ab1067e6da070
nefrttUnknown <39979965+nefrttPrabhu@users.noreply.github.com>
2024-11-28 18:36:36 +0530

    Create preorder.cpp

---
+++ 77_tree/preorder.cpp
@@ -0,0 +1,7 @@
+void preorder() // node left right jiski jitni importance utna phle vo as per the name here we have pre: node highest then left then right 
+{
+    if(node==NULL) // recusrion se pura tree traverse kar rhe
+    return node;
+    preorder(node->left); // pura traverse jabtak null nhi milta
+    preorder(node->right); // pura left traverse hone ke baad right pura jabtak null nhi milta
+}
