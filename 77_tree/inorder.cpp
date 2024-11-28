55e0e68e60e6cec56e7186917db649568195811c
nefrttUnknown <39979965+nefrttPrabhu@users.noreply.github.com>
2024-11-28 18:40:24 +0530

    Update inorder.cpp

--- 77_tree/inorder.cpp
+++ 77_tree/inorder.cpp
@@ -1,1 +1,18 @@
+/*
+          1
+         / \
+        2   3
+         \
+          5
+      output: 2 5 1 3
+*/
+
+void inorder(node* root) {
+    if (root == NULL) // Base condition to stop recursion
+        return;
+
+    inorder(root->left);       // Traverse the left subtree first
+    cout << root->data << " "; // Visit the current node
+    inorder(root->right);      // Traverse the right subtree
+}
 
