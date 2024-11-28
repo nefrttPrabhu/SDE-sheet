1008ed27744c7df93391bce83c778d1a55087bd9
nefrttUnknown <39979965+nefrttPrabhu@users.noreply.github.com>
2024-11-29 01:44:20 +0530

    Update BT traversal.cpp

--- 80_level traversal/BT traversal.cpp
+++ 80_level traversal/BT traversal.cpp
@@ -3,7 +3,8 @@ public:
     vector<vector<int>> levelOrder(TreeNode* root) 
 {
         vector<vector<int>> ans; // To store the final result
-        if (root == NULL) return ans; // If root is NULL, return an empty vector
+        if (root == NULL) 
+            return ans; // If root is NULL, return an empty vector
         
         queue<TreeNode*> q; // Queue to facilitate level-order traversal
         q.push(root); // Start with the root node
@@ -11,7 +12,7 @@ public:
         while (!q.empty()) // While there are nodes to process
         { 
             int size = q.size(); // Get the number of nodes at the current level
-            vector<int> level; // Temporary vector to store values of the current level
+            vector<int> level; // Temporary vector to store values of the current level jo ans mai add karta jayega
             
             for (int i = 0; i < size; i++)  // Process all nodes at the current level
             {
@@ -19,15 +20,14 @@ public:
                 q.pop(); // Remove it from the queue
                 level.push_back(node->val); // Add the node's value to the current level vector
                 
-                // If the node has a left child, add it to the queue
-                if (node->left != NULL) q.push(node->left);
-                // If the node has a right child, add it to the queue
-                if (node->right != NULL) q.push(node->right);
+                if (node->left != NULL) 
+                    q.push(node->left); // If the node has a left child, add it to the queue
+                
+                if (node->right != NULL) 
+                    q.push(node->right); // If the node has a right child, add it to the queue
             }
-            
             ans.push_back(level); // Add the current level to the final result
         }
-        
         return ans; // Return the level order traversal
     }
 };
