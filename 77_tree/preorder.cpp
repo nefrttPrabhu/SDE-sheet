521ef3ec6d336a5760ae9d6f7964ef3c0f2c92e6
nefrttUnknown <39979965+nefrttPrabhu@users.noreply.github.com>
2024-11-29 19:00:42 +0530

    Update preorder.cpp

--- 77_tree/preorder.cpp
+++ 77_tree/preorder.cpp
@@ -9,7 +9,7 @@ output: 1 2 3 5 3
 */
 
 
-void preorder(node) // node left right jiski jitni importance utna phle vo as per the name here we have pre: node highest then left then right 
+void preorder(node* root) // node left right jiski jitni importance utna phle vo as per the name here we have pre: node highest then left then right 
 {
     if(node==NULL) // recusrion se pura tree traverse kar rhe
     return node;
