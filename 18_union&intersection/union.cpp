949e2d8b53aac328040bce0b2bd2132e554440a0
nefrttUnknown <39979965+nefrttPrabhu@users.noreply.github.com>
2024-11-30 14:38:55 +0530

    Update union.cpp

--- 18_union&intersection/union.cpp
+++ 18_union&intersection/union.cpp
@@ -4,6 +4,11 @@ int arr2[] = {2, 3, 4, 4, 5, 11, 12};
 Output:
 Union of arr1 and arr2 is but we dont repeat any element
 1 2 3 4 5 6 7 8 9 10 11 12. 
+
+int arr1[] = {1,2,2,3,4,5};
+int arr2[] = {1,2,3,3,5,6};
+Output:
+{1,2,3,4,5,6}
 */
 
 
@@ -13,32 +18,33 @@ using namespace std;
 
 vector < int > FindUnion(int arr1[], int arr2[], int n, int m) {
   int i = 0, j = 0; // pointers
-  vector < int > Union; // Uninon vector
-  while (i < n && j < m) {
-    if (arr1[i] <= arr2[j]) // Case 1 and 2
+  vector < int > Union; // Uninon vecto
+  while (i < n && j < m) //jab tak i and j both have elements
+  {
+    if (arr1[i] <= arr2[j]) //arr1 ka element chota hi ya fir equal hi arr2 ke element se eg:1
     {
-      if (Union.size() == 0 || Union.back() != arr1[i])
-        Union.push_back(arr1[i]);
-      i++;
+      if (Union.size() == 0 || Union.back() != arr1[i]) //agar khali hi ya fir abhi tak vo present he nhi hi 
+        Union.push_back(arr1[i]); // toh dal do
+      i++; // j wahin hi i aage badha hi 
     } 
-    else // case 3
+    else // idhar check ho rha only for arr2 greater hi arr2 ke element se
     {
-      if (Union.size() == 0 || Union.back() != arr2[j])
-        Union.push_back(arr2[j]);
-      j++;
+      if (Union.size() == 0 || Union.back() != arr2[j]) //agar khali hi ya fir abhi tak vo present he nhi hi 
+        Union.push_back(arr2[j]); // toh dal do
+      j++; // i wahin hi j aage badha hi
     }
   }
-  while (i < n) // IF any element left in arr1
+  while (i < n) //j exhauseted now only remaining is i elements
   {
     if (Union.back() != arr1[i])
       Union.push_back(arr1[i]);
-    i++;
+    i++; // j toh khtm i ko dalo and badhao
   }
-  while (j < m) // If any elements left in arr2
+  while (j < m) //i exhauseted now only remaining is j elements
   {
     if (Union.back() != arr2[j])
       Union.push_back(arr2[j]);
-    j++;
+    j++; // i toh khtm j ko dalo and badhao
   }
   return Union;
 }
