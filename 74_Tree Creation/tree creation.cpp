7c22641a93135fde079fffa0fa53700713c08017
nefrttUnknown <39979965+nefrttPrabhu@users.noreply.github.com>
2024-12-01 18:48:14 +0530

    Update tree creation.cpp

--- 74_Tree Creation/tree creation.cpp
+++ 74_Tree Creation/tree creation.cpp
@@ -10,13 +10,13 @@
 
 #include <iostream>
 using namespace std;
-struct node // struct type a data coz koi bhi type a data types ko store kar sakta hi 
+class Node // struct type a data coz koi bhi type a data types ko store kar sakta hi 
 {
     int data; // int type stored hi 
-    struct node* left; //pointer type stored hi
-    struct node* right; //pointer type stored hi
+    Node* left; //pointer type stored hi
+    Node* right; //pointer type stored hi
     
-    node(int val) // constructor
+    Node(int val) // constructor
     {
         data = val;         // Assigns the provided value to the node's data field. ye store karega node ki value bina dekhe right hi ya left coz uski jankari left right pointers rakhenge
         left = NULL;        // Initializes the left child to NULL (no child yet). ye store kar rhe ki growth left side hi 
@@ -26,11 +26,15 @@ struct node // struct type a data coz koi bhi type a data types ko store kar sak
 };
 
 int main() {
-    struct node* root=new node(1); // root node define kiya hi with value and ye call kar rha node constructor ko 
-    root ->left=new node(2); //apne se new node create kiya hi, root se start hoke left ki side grow karna
-    root ->right=new node(3); // apne se right node create kiya hi, root se start hoke right ki side grow karna
-    root ->left ->right=new node(5); // apne se left ke right node ko value assign kari hi waise he karte jao as per our need
-    return 0;
+    // Creating the root node with value 1
+    Node* root = new Node(1); // root node define kiya hi with value and ye call kar rha node constructor ko 
+
+    // Adding children to the root
+    root->left = new Node(2); //apne se new node create kiya hi, root se start hoke left ki side grow karna
+    root->right = new Node(3); // apne se right node create kiya hi, root se start hoke right ki side grow karna
+
+    // Adding a child to the left node
+    root->left->right = new Node(5); // apne se left ke right node ko value assign kari hi waise he karte jao as per our need
 }
 
 
