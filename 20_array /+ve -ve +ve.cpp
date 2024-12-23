d260ea288e28d838fcc424540d6a208b3eab7f5c
nefrttUnknown <39979965+nefrttPrabhu@users.noreply.github.com>
2024-12-23 18:48:56 +0530

    Update and rename 20_arrays to 20_array /+ve -ve +ve.cpp

---
+++ 20_array /+ve -ve +ve.cpp
@@ -0,0 +1,94 @@
+
+/*
+Input:
+arr[] = {1,2,-3,-1,-2,-3}
+Output: 1 -3 2 -1 3 -2
+
+*/
+//Variety having equal negative and positive
+//brute force
+  vector<int> pos;
+  vector<int> neg;
+  // Segregate the array into positives and negatives.
+  for(int i=0;i<n;i++){
+      
+      if(A[i]>0) pos.push_back(A[i]);
+      else neg.push_back(A[i]);
+  }
+
+//optimal 
+  vector<int> ans(n,0);
+  
+  // positive elements start from 0 and negative from 1.
+  int posIndex = 0, negIndex = 1;
+  for(int i = 0;i<n;i++){
+      
+      // Fill negative elements in odd indices and inc by 2.
+      if(A[i]<0){
+          ans[negIndex] = A[i];
+          negIndex+=2;
+      }
+      
+      // Fill positive elements in even indices and inc by 2.
+      else{
+          ans[posIndex] = A[i];
+          posIndex+=2;
+      }
+  }
+
+
+
+
+// variety 2 if neg and pos are not equal
+  vector<int> pos;
+  vector<int> neg;
+  
+  // Segregate the array into positives and negatives.
+  for(int i=0;i<n;i++){
+      
+      if(A[i]>0) pos.push_back(A[i]);
+      else neg.push_back(A[i]);
+  }
+  
+  // If positives are lesser than the negatives.
+  if(pos.size() < neg.size()){
+      
+    // First, fill array alternatively till the point 
+    // where positives and negatives ar equal in number.
+    for(int i=0;i<pos.size();i++){
+      
+      A[2*i] = pos[i];
+      A[2*i+1] = neg[i];
+    }
+    
+    // Fill the remaining negatives at the end of the array.
+    int index = pos.size()*2;
+    for(int i = pos.size();i<neg.size();i++){
+        
+        A[index] = neg[i];
+        index++;
+    }
+  }
+  
+  // If negatives are lesser than the positives.
+  else{
+      
+      // First, fill array alternatively till the point 
+      // where positives and negatives ar equal in number.
+      for(int i=0;i<neg.size();i++){
+      
+      A[2*i] = pos[i];
+      A[2*i+1] = neg[i];
+  }
+    
+    // Fill the remaining positives at the end of the array.
+    int index = neg.size()*2;
+    for(int i = neg.size();i<pos.size();i++){
+        
+        A[index] = pos[i];
+        index++;
+    }
+  }
+  return A;
+    
+}
