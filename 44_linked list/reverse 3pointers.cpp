4220a46ac47b3e2ef23545afc30ef5eceb16fba8
nefrttUnknown <39979965+nefrttPrabhu@users.noreply.github.com>
2024-12-28 14:46:10 +0530

    Update reverse 3pointers.cpp

--- 44_linked list/reverse 3pointers.cpp
+++ 44_linked list/reverse 3pointers.cpp
@@ -1,19 +1,14 @@
-
+// Function to reverse the linked list
 void reverseLinkedList(Node* &head) {
-    Node* prev = NULL; // Initially, there is no previous node
-    Node* curr = head; // Start with the head of the list
-    Node* next = NULL; // Temporary pointer to store the next node
+    Node* prev = NULL;  // Initially, there is no previous node
+    Node* front = head; // Start with the head of the list
+    Node* curr = NULL;  // Temporary pointer to store the current node
 
-    while (curr != NULL) {
-        // Store the next node
-        next = curr->next;
-
-        // Reverse the current node's pointer
-        curr->next = prev;
-
-        // Move pointers forward
-        prev = curr;     // Move prev to the current node
-        curr = next;     // Move curr to the next node
+    while (front != NULL) {
+        curr = front;       // Store the current node
+        front = front->next; // Move to the next node
+        curr->next = prev;   // Reverse the current node's pointer
+        prev = curr;         // Move the `prev` pointer to the current node
     }
 
     // Update head to the new first node
