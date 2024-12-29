ac7a62739071ec6338504d0d4c5001022a5dbcc7
nefrttUnknown <39979965+nefrttPrabhu@users.noreply.github.com>
2024-12-29 12:30:01 +0530

    Update finding middle.cpp

--- 44_linked list/finding middle.cpp
+++ 44_linked list/finding middle.cpp
@@ -1,1 +1,9 @@
-
+int* middle(Node* head){
+  Node* slow=head;
+  Node* fast=head;
+  while(fast->next!=NULL && fast->next->next!=NULL){
+    fast=fast->next->next;
+    slow=slow->next;
+  }
+  return mid;
+}
