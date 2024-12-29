edc0226b0b575291a1ecea14189968280aa108b4
nefrttUnknown <39979965+nefrttPrabhu@users.noreply.github.com>
2024-12-29 17:59:14 +0530

    Update even&odd index.cpp

--- 44_linked list/Questions/even&odd index.cpp
+++ 44_linked list/Questions/even&odd index.cpp
@@ -1,1 +1,23 @@
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if (head == NULL || head->next == NULL) {
+            return head; // Return the list as is if it's empty or has a single node
+        }
 
+        ListNode* odd = head; // Pointer for odd-indexed nodes
+        ListNode* even = head->next; // Pointer for even-indexed nodes
+        ListNode* evenHead = head->next; // Save the head of the even list for later connection
+
+        while (even != NULL && even->next != NULL) {
+            odd->next = odd->next->next; // Link current odd node to the next odd node
+            even->next = even->next->next; // Link current even node to the next even node
+
+            odd = odd->next; // Move odd pointer to the next odd node
+            even = even->next; // Move even pointer to the next even node
+        }
+
+        odd->next = evenHead; // Connect the end of the odd list to the head of the even list
+        return head; // Return the modified list
+    }
+};
