bec5dfcfb9ca4d2d9e629f72f8e0b6126bbf1f2d
nefrttUnknown <39979965+nefrttPrabhu@users.noreply.github.com>
2024-12-31 21:32:21 +0530

    Update reverse recursion.cpp

--- 44_linked list/reverse recursion.cpp
+++ 44_linked list/reverse recursion.cpp
@@ -16,12 +16,13 @@ Node* reverseLinkedList(Node* head) {
     Node* newHead = reverseLinkedList(head->next); //I1: 4 coz head=5 se toh bheja he tha aise karte karte last head->[1] jaeyga but head->NULL hi toh return [1] hoga jo ki new head banega then neeche wale waiting cases execute honge
     
     // Save a reference to the node following
-    // the current 'head' node. //I1:head->[5]->[4]->[3]->[2]->[1]->NULL I2:head->[4]->[3]->[2]->[1]->NULL I3:head->[3]->[2]->[1]->NULL I4:head->[2]->[1]->NULL ...I4 will be executed 1st then again list links will be changed for I3 then again for I2 again for I1 entirly
-    Node* front = head->next; //I2:NULL<-[2]<-[1]<-newHead I3:NULL<-[3]<-[2]<-[1]<-newHead I4:NULL<-[4]<-[3]<-[2]<-[1]<-newHead I5:NULL<-[5]<-[4]<-[3]<-[2]<-[1]<-newHead
+    // the current 'head' node. //I1:head->[5]->[4]->[3]->[2]->[1]->NULL I2:head->[4]->[3]->[2]->[1]->NULL I3:head->[3]->[2]->[1]->NULL I4:head->[2]->[1]->NULL ...I5 will be executed 1st then again list links will be changed for I4 then for I3
+    Node* front = head->next; 
     
     // Make the 'front' node point to the current
     // 'head' node in the reversed order.
-    front->next = head;
+                        //          recusive call   h    f               h    f                    h    f                         h    f
+    front->next = head; //I5:NULL<-[1]<-newHead I4:[2]->[1]<-newHead I3:[3]->[2]<-[1]<-newHead I2:[4]->[3]<-[2]<-[1]<-newHead I1:[5]->[4]<-[3]<-[2]<-[1]<-newHead
     
     // Break the link from the current 'head' node
     // to the 'front' node to avoid cycles.
@@ -29,7 +30,7 @@ Node* reverseLinkedList(Node* head) {
     
     // Return the 'newHead,' which is the new
     // head of the reversed linked list.
-    return newHead; // ye hamesha wahi last wale node ko return kar rha hi as the newHead ie [1]
+    return newHead; // ye hamesha wahi last wale node ko return kar rha hi as the newHead ie [1] next //I5:NULL<-[1]<-newHead I4:NULL<-[2]<-[1]<-newHead I3:NULL<-[3]<-[2]<-[1]<-newHead I2:NULL<-[4]<-[3]<-[2]<-[1]<-newHead I1:NULL<-[5]<-[4]<-[3]<-[2]<-[1]<-newHead
 }
 
 
