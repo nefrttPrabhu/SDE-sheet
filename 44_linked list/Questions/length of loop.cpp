af7ba9bbf13e7d023bdb7eefef8af4dd12ad96e3
nefrttUnknown <39979965+nefrttPrabhu@users.noreply.github.com>
2025-01-01 13:22:29 +0530

    Update length of loop.cpp

--- 44_linked list/Questions/length of loop.cpp
+++ 44_linked list/Questions/length of loop.cpp
@@ -1,1 +1,47 @@
+//M1: brute force
+// Function to return the length 
+// of loop using hashing
+
+int lengthOfLoop(Node* head) {
+    // Hashmap to store visited
+    // nodes and their timer values
+    unordered_map<Node*, int> visitedNodes; 
+    
+    // Initialize pointer to
+    // traverse the linked list
+    Node* temp = head; 
+    
+    // Initialize timer to track
+    // visited nodes
+    int timer = 0; 
+
+    // Traverse the linked list till
+    // temp reaches nullptr
+    while (temp != NULL) {
+        
+        // If revisiting a node return
+        // the difference of timer values
+        if (visitedNodes.find(temp) != visitedNodes.end()) {
+            // Calculate the length of the loop
+            int loopLength = timer - visitedNodes[temp];
+            
+            // Return the length of the loop
+            return loopLength; 
+        }
+        // Store the current node and
+        // its timer value in the hashmap
+        visitedNodes[temp] = timer;
+        
+        // Move to the next node
+        temp = temp->next;
+        
+         // Increment the timer
+        timer++;
+    }
+
+    // If traversal is completed and
+    // we reach the end of the list (null)
+    // means there is no loop
+    return 0;
+}
 
