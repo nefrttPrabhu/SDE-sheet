8d9f2db7432c53a1d11240bc5110dfe6b54f8dad
nefrttUnknown <39979965+nefrttPrabhu@users.noreply.github.com>
2025-01-04 23:26:37 +0530

    Update rotate LL.cpp

--- 44_linked list/Questions/rotate LL.cpp
+++ 44_linked list/Questions/rotate LL.cpp
@@ -1,1 +1,36 @@
 
+// Utility function to insert node at the end of the list
+void insertNode(node* &head, int val) {
+    node* newNode = new node(val);
+    if (head == NULL) {
+        head = newNode;
+        return;
+    }
+    node* temp = head;
+    while (temp->next != NULL) temp = temp->next;
+    temp->next = newNode;
+}
+
+// Utility function to rotate list by k times
+node* rotateRight(node* head, int k) {
+    if (head == NULL || head->next == NULL) return head;
+    for (int i = 0; i < k; i++) {
+        node* temp = head;
+        while (temp->next->next != NULL) 
+          temp = temp->next;
+        node* end = temp->next;
+        temp->next = NULL;
+        end->next = head;
+        head = end;
+    }
+    return head;
+}
+
+// Utility function to print list
+void printList(node* head) {
+    while (head->next != NULL) {
+        cout << head->num << "->";
+        head = head->next;
+    }
+    cout << head->num << endl;
+}
