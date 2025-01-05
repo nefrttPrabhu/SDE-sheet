f7b4f36d4c326f1211245f065ca4629e3c1ae29e
nefrttUnknown <39979965+nefrttPrabhu@users.noreply.github.com>
2025-01-05 22:29:52 +0530

    Update factorial.cpp

--- 30_Recursion/factorial.cpp
+++ 30_Recursion/factorial.cpp
@@ -1,1 +1,17 @@
+//no return type
+void factorialHelper(int n, int& result) {
+    if (n == 0 || n == 1) {
+        result = 1; // Base case
+        return;
+    }
+    result *= n; // Multiply result by the current number
+    factorialHelper(n - 1, result); // Recursive call
+}
 
+//int return type
+int factorial(int n) {
+    if (n == 0 || n == 1) {
+        return 1; // Base case: factorial of 0 or 1 is 1
+    }
+    return n * factorial(n - 1); // Recursive call
+}
