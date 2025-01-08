2c2f847f10e1195ab96e5a00c4423108a83c5532
nefrttUnknown <39979965+nefrttPrabhu@users.noreply.github.com>
2025-01-09 02:09:10 +0530

    Update KMP algorithm.cpp

--- 200_DP/KMP algorithm.cpp
+++ 200_DP/KMP algorithm.cpp
@@ -1,3 +1,9 @@
+/*
+Enter the text: ababcabcabababd
+Enter the pattern: abababd
+Pattern found at index 10
+
+*/
 // Function to compute the LPS (Longest Prefix Suffix) array
 void computeLPSArray(const string& pattern, vector<int>& lps) {
     int p = 0; // Prefix pointer
@@ -7,7 +13,7 @@ void computeLPSArray(const string& pattern, vector<int>& lps) {
     while (s < pattern.length()) {
         if (pattern[s] == pattern[p]) {
             p++;
-            lps[s] = p;
+            lps[s] = p; // idha rhoga ye ki p start mai zero hi toh agar ye uper likhte and uper wali line neeche then [0, 0, 0, 1, 2, 3, 0] instead of [0, 0, 1, 2, 3, 4, 0]
             s++;
         } else {
             if (p != 0) {
