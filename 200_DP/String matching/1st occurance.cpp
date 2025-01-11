f2d68195411402a208e396335bedcd3188e64825
nefrttUnknown <39979965+nefrttPrabhu@users.noreply.github.com>
2025-01-12 01:56:34 +0530

    Update 1st occurance.cpp

--- 200_DP/String matching/1st occurance.cpp
+++ 200_DP/String matching/1st occurance.cpp
@@ -14,3 +14,19 @@ for (i = 0; i <= n - m; i++) {  // Loop through the text, ensuring there's enoug
     }
 }
 return -1;                      // Return -1 if no match is found
+
+
+//M2:
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int l1 = haystack.size();
+        int l2 = needle.size();
+        for (int i = 0; i <= l1 - l2; i++) {
+            if (haystack.substr(i, l2) == needle) {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
