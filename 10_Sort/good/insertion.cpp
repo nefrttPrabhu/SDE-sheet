6e89f394d4b50ad4091eb74a9d34c711e11f3c91
nefrrttUnknown <39979965+nefrttPrabhu@users.noreply.github.com>
2025-01-23 01:56:59 +0530

    Update insertion.cpp

--- 10_Sort/good/insertion.cpp
+++ 10_Sort/good/insertion.cpp
@@ -17,9 +17,17 @@ using namespace std;
 void insertion_sort(int arr[], int n) {
     for (int i = 0; i <= n - 1; i++) {
         for (int j = i; (j > 0 && arr[j - 1] > arr[j]); j--) {
-            int temp = arr[j - 1];
-            arr[j - 1] = arr[j];
-            arr[j] = temp;
+            swap(a[j - 1], a[j]);
         }
+    }
+}
+
+
+//better
+for (int i = 0; i <= n - 1; i++) {
+    int j = i;
+    while (j > 0 && a[j - 1] > a[j]) {
+        swap(a[j - 1], a[j]);
+        j--;
     }
 }
