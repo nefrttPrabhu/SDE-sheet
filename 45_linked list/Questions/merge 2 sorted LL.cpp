5b02b2abe1a4d818aecbf8f3c5bd505fb218d61e
nefrrttUnknown <39979965+nefrttPrabhu@users.noreply.github.com>
2025-01-26 01:21:39 +0530

    Update merge 2 sorted LL.cpp

--- 45_linked list/Questions/merge 2 sorted LL.cpp
+++ 45_linked list/Questions/merge 2 sorted LL.cpp
@@ -1,28 +1,4 @@
-/*
-  M1: brute force 
-  S1: get all the node values stored in array
-  S2: sort the array 
-  S3: overwrite the elements of the LL
-*/
-Node* sortLL(Node* head){
-    vector<int> arr;
-    Node* temp = head;    // Temporary pointer to & traverse the linked list
-    while(temp!=NULL){
-        arr.push_back(temp->data);
-        temp = temp->next;
-    }
-    sort(arr.begin(), arr.end()); // Sort the array & containing node values
-    temp = head;     // Reassign sorted values to & the linked list nodes
-    for(int i = 0; i < arr.size(); i++){
-        temp->data = arr[i];  // Update the node's data & with the sorted values
-        temp = temp->next;  // Move to the next node
-    }
-    return head;     // Return the head of the & sorted linked list
-}
-
-
-
-//M2: optimal extensive use of pointers only using a dummy node
+//M1: optimal extensive use of pointers only using a dummy node
 Node* mergeTwoSortedLinkedLists(Node* list1, Node* list2) {
     Node* dummyNode = new Node(-1);
     Node* temp = dummyNode;
@@ -46,42 +22,50 @@ Node* mergeTwoSortedLinkedLists(Node* list1, Node* list2) {
 
 
 
-//M3:
+//M2:
 Node* mergeTwoSortedLinkedLists(Node* list1, Node* list2) {
-Node* head, *h1, *h2; // Corrected pointer declarations with proper initialization
-if (t1->data < t2->data) {
-    head = t1;
-    h1 = t1; // Corrected: Assign h1 to t1 to keep track of list1
-    t1 = t1->next;
-    h1->next = t2; // Corrected: Use -> instead of . for accessing members of a pointer
-} else {
-    head = t2;
-    h2 = t2; // Corrected: Assign h2 to t2 to keep track of list2
-    t2 = t2->next;
-    h2->next = t1; // Corrected: Use -> instead of . for accessing members of a pointer
-}
-
-// Remaining list
-while (t1 != nullptr && t2 != nullptr) { // Corrected: Use nullptr instead of null
-    if (t1->data < t2->data) {
-        Node* temp = t1->next; // Declared temp as Node* for type safety
-        t1->next = t2;         // Corrected: Use -> instead of .
-        t1 = temp;
+    Node* head = nullptr, *h1 = nullptr, *h2 = nullptr; 
+    Node *t1 = list1, *t2 = list2;  // Define t1 and t2 for traversal
+    
+    if (t1 && t2) { // Check if both lists are non-empty
+        if (t1->data < t2->data) {
+            head = t1;
+            h1 = t1; // Assign h1 to t1 to keep track of list1
+            t1 = t1->next;
+            h1->next = t2; // Link h1 to t2 to continue merging
+        } else {
+            head = t2;
+            h2 = t2; // Assign h2 to t2 to keep track of list2
+            t2 = t2->next;
+            h2->next = t1; // Link h2 to t1 to continue merging
+        }
     } else {
-        Node* temp = t2->next; // Declared temp as Node* for type safety
-        t2->next = t1;         // Corrected: Use -> instead of .
-        t2 = temp;
-    }
-}
-
-// dono lists mai se koi bhi ek bach gyi even if anyone get exhausted
-    if (list1 != nullptr) {
-        temp->next = list1;
-    } else {
-        temp->next = list2;
+        // Handle the case where one of the lists is initially empty
+        return (t1 != nullptr) ? t1 : t2;
     }
 
-  return head;
+    // Merge remaining nodes
+    Node* temp = nullptr;
+    while (t1 != nullptr && t2 != nullptr) {
+        if (t1->data < t2->data) {
+            temp = t1->next; // Store the next node
+            t1->next = t2;   // Link t1 to t2
+            t1 = temp;       // Move t1 to the next node
+        } else {
+            temp = t2->next; // Store the next node
+            t2->next = t1;   // Link t2 to t1
+            t2 = temp;       // Move t2 to the next node
+        }
+    }
+
+    // If one list is exhausted, attach the remaining part of the other list
+    if (t1 != nullptr) {
+        temp->next = t1; // Link the last node to the remaining list
+    } else {
+        temp->next = t2; // Link the last node to the remaining list
+    }
+
+    return head;
 }
 
 
