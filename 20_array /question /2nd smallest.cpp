789766036efdfab6405b6870053ecbfb2409a720
nefrrttUnknown <39979965+nefrttPrabhu@users.noreply.github.com>
2025-01-29 01:11:07 +0530

    Update 2nd smallest.cpp

--- 20_array /question /2nd smallest.cpp
+++ 20_array /question /2nd smallest.cpp
@@ -1,4 +1,6 @@
- for (int i = 0; i < n; i++) {
+//Eg: {1, 2, 3, 4, 3, 2, 1}
+
+for (int i = 0; i < n; i++) {
         if (a[i] < smallest) {
             secondSmallest = smallest; // Update second smallest
             smallest = a[i];           // Update smallest
