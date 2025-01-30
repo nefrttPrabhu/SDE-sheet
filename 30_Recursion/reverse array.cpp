22ebfe0ef1da8cde112c73c1fd563e2ce29e2749
nefrrttUnknown <39979965+nefrttPrabhu@users.noreply.github.com>
2025-01-30 22:06:14 +0530

    Update reverse array.cpp

--- 30_Recursion/reverse array.cpp
+++ 30_Recursion/reverse array.cpp
@@ -1,8 +1,18 @@
 //M1:
-void fng(int i, int j, int nums[]) {
+void fng(int i, int j) {
     if (i >= j)  // Base condition
         return;
     
     swap(nums[i], nums[j]);  // Swap elements
-    fng(i + 1, j - 1, nums); // Recursive call
+    fng(i + 1, j - 1); // Recursive call
+}
+
+
+//M2:
+void fng(int i) {
+    if (i >= n/2)  // Base condition
+        return;
+    
+    swap(nums[i], nums[n - i - 1]);  // Swap elements
+    fng(i + 1); // Recursive call
 }
