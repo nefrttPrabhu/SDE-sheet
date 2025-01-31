a3b3978eef5e08b30ca6f6a3798d1c589ee00ba9
nefrrttUnknown <39979965+nefrttPrabhu@users.noreply.github.com>
2025-02-01 01:37:57 +0530

    Update combination sum2.cpp

--- 30_Recursion/questions/combination sum2.cpp
+++ 30_Recursion/questions/combination sum2.cpp
@@ -1,3 +1,4 @@
+
 #include <vector>
 #include <algorithm>
 using namespace std;
@@ -19,7 +20,7 @@ public:
         }
         for (int i = start; i < candidates.size(); ++i) {
             if (i > start && candidates[i] == candidates[i - 1]) // i> start ye check kar rha ki hum aage badhe hi ki ni as i==start when I1 and in I2 i will be i++ but start remains same and agar ha toh fir ye dekho ki uske phele wala ie i-1th index wala bhi same hi ith wale ke toh usko fir se iteration mai dalne se duplicate ka error ayega, isiliye hum remove kar rhe 
-            continue;                                            //Eg: [1,1,1,2,2]                                   
+            continue;                                            //Eg: [1,1,1,2,2]  //isse hoga ye ki agar ith index wala ko jab phele he reject kar diya hi and uske baad wala ie i+1th index wala bhi same hi toh usko fir se iteration mai dalna time waste hoga toh isiliye hum remove kar rhe                     
             if (candidates[i] > target) break; 
             combination.push_back(candidates[i]);
             backtrack(candidates, target - candidates[i], i + 1, combination, result);
