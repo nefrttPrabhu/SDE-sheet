c4efe3a9ae9d4efe3b143ec04e67340acb3be988
nefrrttUnknown <39979965+nefrttPrabhu@users.noreply.github.com>
2025-02-01 23:48:30 +0530

    Update combination sum2.cpp

--- 30_Recursion/questions/combination sum2.cpp
+++ 30_Recursion/questions/combination sum2.cpp
@@ -20,9 +20,11 @@ public:
         }
         for (int i = start; i < candidates.size(); ++i) {
             if (i > start && candidates[i] == candidates[i - 1]) // i> start ye check kar rha ki hum aage badhe hi ki ni as i==start when I1 and in I2 i will be i++ but start remains same and agar ha toh fir ye dekho ki uske phele wala ie i-1th index wala bhi same hi ith wale ke toh usko fir se iteration mai dalne se duplicate ka error ayega, isiliye hum remove kar rhe 
-            continue;                                            //Eg: [1,1,1,2,2]  //isse hoga ye ki agar ith index wala ko jab phele he reject kar diya hi and uske baad wala ie i+1th index wala bhi same hi toh usko fir se iteration mai dalna time waste hoga toh isiliye hum remove kar rhe                     
-            if (candidates[i] > target) break; 
+                continue;                                        //Eg: [1,1,1,2,2]  //isse hoga ye ki agar ith index wala ko jab phele he reject kar diya hi and uske baad wala ie i+1th index wala bhi same hi toh usko fir se iteration mai dalna time waste hoga toh isiliye hum remove kar rhe                     
+            if (candidates[i] > target) 
+                break; //phele he given arrray sorted hi toh fir agar next element agar target(remaining) se he bada hi toh uske aage wale bhi honge coz of sorted array
             combination.push_back(candidates[i]);
+                      //given array , new target        , start, recent set , storing the valid sets
             backtrack(candidates, target - candidates[i], i + 1, combination, result);
             combination.pop_back(); 
         }
