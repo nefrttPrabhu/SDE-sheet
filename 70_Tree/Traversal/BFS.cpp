d2d5ac0a004010c592a64d6d2524525369cd644f
nefrrttUnknown <39979965+nefrttPrabhu@users.noreply.github.com>
2025-02-03 02:07:24 +0530

    Update BFS.cpp

--- 70_Tree/Traversal/BFS.cpp
+++ 70_Tree/Traversal/BFS.cpp
@@ -3,14 +3,8 @@ S1. define queue
 S2. create a visited array
 S3. adjecency list
 */
-
-#include <bits/stdc++.h>
-using namespace std;
-
-class Solution {
-  public:
     // Function to return Breadth First Traversal of given graph.
-    vector<int> bfsOfGraph(int V, vector<int> adj[]) {
+    vector<int> BFS(int V, vector<int> adj[]) {
         int vis[V] = {0}; 
         vis[0] = 1; 
         queue<int> q;
@@ -39,29 +33,25 @@ class Solution {
     }
 };
 
-void addEdge(vector <int> adj[], int u, int v) {
-    adj[u].push_back(v);
-    adj[v].push_back(u);
-}
 
-void printAns(vector <int> &ans) {
-    for (int i = 0; i < ans.size(); i++) {
-        cout << ans[i] << " ";
-    }
-}
 
-int main() 
-{
-    vector <int> adj[6];
+
+//M2:
+void BFS(Node* root) {
+    if (root == nullptr) return;
+
+    queue<Node*> q;
+    vector<int> bfs_result;
+
+    q.push(root);
     
-    addEdge(adj, 0, 1);
-    addEdge(adj, 1, 2);
-    addEdge(adj, 1, 3);
-    addEdge(adj, 0, 4);
-
-    Solution obj;
-    vector <int> ans = obj.bfsOfGraph(5, adj);
-    printAns(ans);
-
-    return 0;
+    while (!q.empty()) {
+        Node* node = q.front();
+        q.pop();
+        
+        bfs_result.push_back(node->data); // Store node value instead of pointer
+        
+        if (node->left)  q.push(node->left);  // Push left child first
+        if (node->right) q.push(node->right); // Push right child next
+    }
 }
