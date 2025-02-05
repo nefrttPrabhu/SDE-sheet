3baac901f53c62971ec1c02504075ebf1d685f33
nefrrttUnknown <39979965+nefrttPrabhu@users.noreply.github.com>
2025-02-06 00:38:21 +0530

    Update count leaf nodes.cpp

--- 70_Tree/concept/count leaf nodes.cpp
+++ 70_Tree/concept/count leaf nodes.cpp
@@ -1,1 +1,10 @@
+int countLeafNodes(Node* root) {
+    if (root == nullptr) 
+        return 0;
+    
+    if (root->left == nullptr && root->right == nullptr)
+        return 1;
+
+    return countLeafNodes(root->left) + countLeafNodes(root->right);
+}
 
