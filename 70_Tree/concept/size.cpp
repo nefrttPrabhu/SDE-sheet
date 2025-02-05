8ebec3b94d7e98060c0d7c38f409ca196cd697b9
nefrrttUnknown <39979965+nefrttPrabhu@users.noreply.github.com>
2025-02-06 00:51:21 +0530

    Update size.cpp

--- 70_Tree/concept/size.cpp
+++ 70_Tree/concept/size.cpp
@@ -1,1 +1,9 @@
-
+int sizeOfBinaryTree(Node* root) {
+    if (root == nullptr) {
+        return 0;
+    }
+    int leftSize = sizeOfBinaryTree(root->left);
+    int rightSize = sizeOfBinaryTree(root->right);
+    
+    return leftSize + rightSize + 1;
+}
