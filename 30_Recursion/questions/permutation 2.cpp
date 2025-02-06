e3315ec47d268ea1404e3d0d5c4149f560d45427
nefrrttUnknown <39979965+nefrttPrabhu@users.noreply.github.com>
2025-02-06 16:36:52 +0530

    Update permutation 2.cpp

--- 30_Recursion/questions/permutation 2.cpp
+++ 30_Recursion/questions/permutation 2.cpp
@@ -1,1 +1,12 @@
+void generatePermutations(vector<int> &a, int index, vector<vector<int>> &ans) {
+    if (index == a.size()) {  // Base case: If index reaches the end, store the permutation
+        ans.push_back(a);
+        return;
+    }
 
+    for (int i = index; i < a.size(); i++) {
+        swap(a[index], a[i]);  // Swap to create a new permutation
+        generatePermutations(a, index + 1, ans);
+        swap(a[index], a[i]);  // Backtrack to the previous state
+    }
+}
