71d048acd154c7f9ba2226a288d7969689aca054
nefrrttUnknown <39979965+nefrttPrabhu@users.noreply.github.com>
2025-02-10 23:44:23 +0530

    Update Single occurring element.cpp

--- 25_Binary search /questions /Single occurring element.cpp
+++ 25_Binary search /questions /Single occurring element.cpp
@@ -87,3 +87,40 @@ int singleNonDuplicate(int a[], int n) {
 }
 
 
+
+
+//M4:
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int low = 0;
+        int high = nums.size() - 1;
+
+        int n = nums.size();
+        
+        if ( n == 1 ) return nums[0];
+
+        if ( nums[0] != nums[1] ) return nums[0];
+
+        if ( nums[n-2] != nums[n-1] ) return nums[n-1];
+
+        while(low <= high) {
+            int mid = (low + high) / 2;
+
+            if(nums[mid - 1] != nums[mid] && nums[mid + 1] != nums[mid]) {
+                return nums[mid];
+            } 
+//take five numbers index will be from 0 - 4
+            else if(mid % 2 && nums[mid - 1] == nums[mid] ||    //if mid is odd means single element will be on the left of the mid 
+                    mid % 2 == 0 && nums[mid] == nums[mid + 1]) //if mid is even means remaining are even number of numbers to the right of mid so if mid and adjsecnt is equal means the single is on the right of mid + 1
+                    {
+                low = mid + 1;
+            } 
+            
+            else {
+                high = mid - 1;
+            }
+        }
+        return 0;
+    }
+};
