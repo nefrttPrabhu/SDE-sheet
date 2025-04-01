32edaeeeb05ed9fec6772772509fce14ad2a60c2
nefrrttUnknown <39979965+nefrttPrabhu@users.noreply.github.com>
2025-04-01 05:33:05 +0530

    Update DFS.cpp

--- 80_graphs /DFS.cpp
+++ 80_graphs /DFS.cpp
@@ -1,1 +1,72 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
 
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    void dfs(vector<vector<int>>& adj, vector<int>& ls, vector<int>& vis, int node) {
+        vis[node] = 1;
+        ls.push_back(node);
+        
+        for (auto it : adj[node]) {
+            if (!vis[it]) {
+                dfs(adj, ls, vis, it);
+            }
+        }
+    }
+
+    vector<int> dfsOfGraph(vector<vector<int>>& adj) {
+        int n = adj.size();
+        vector<int> ls;
+        vector<int> vis(n, 0);
+
+        int start = 0;
+        dfs(adj, ls, vis, start);
+        
+
+        return ls;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int tc;
+    cin >> tc;
+    cin.ignore();
+    while (tc--) {
+        int V;
+        cin >> V;
+        cin.ignore();
+        vector<vector<int>> adj(
+            V); // Use vector of vectors instead of array of vectors.
+
+        for (int i = 0; i < V; i++) {
+            string input;
+            getline(cin, input);
+            int num;
+            vector<int> node;
+            stringstream ss(input);
+            while (ss >> num) {
+                node.push_back(num);
+            }
+            adj[i] = node;
+        }
+
+        Solution obj;
+        vector<int> ans = obj.dfs(adj);
+        for (int i = 0; i < ans.size(); i++) {
+            cout << ans[i] << " ";
+        }
+        cout << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
